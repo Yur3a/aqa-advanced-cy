2091c682618b4dda13e153c0742450c18cb57565
Yurii Zatochnyi <yurii.zatochnyi@neuraspace.com>
2024-09-23 12:39:42 +0300

    19.1. Cypress assertions, actions and custom commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,31 @@
+const email = "yurii_z@outlook.com";
+const password = "123QWEasd";
+
+Cypress.Commands.add('login', (email, password) => {
+
+
+    cy.get('input[name="email"]').type(email);
+    cy.get('input[name="password"]').type(password);
+    cy.get('button[type="submit"]').click();
+});
+
+Cypress.Commands.overwrite('type', (originalFn, element, text, options) => {
+    if (options && options.sensitive) {
+        options.log = false;
+        Cypress.log({
+            $el: element,
+            name: 'type',
+            message: '*'.repeat(text.length),
+        });
+    }
+
+    return originalFn(element, text, options);
+});
+
+
+
+
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
